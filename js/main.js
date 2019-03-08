ca43cf3fb390e4b126febb5ee2367b9d539f9b16
uday.sharma <uday.sharma@gdn-commerce.com>
2019-03-08 23:21:58 +0530

    js enhancement

--- js/main.js
+++ js/main.js
@@ -3,16 +3,48 @@ window.addEventListener('load', function() {
 })
 
 function init () {
-  attechEvtListnerOnModal()
+  // attechEvtListnerOnOpenModal()
+  // attechEvtListnerOnCloseModal()
+  attachEventListner()
 }
 
-function attechEvtListnerOnModal () {
-  var ele = document.getElementsByName('modal')
-  for (var i = 0; i < ele.length; i++) {
-    ele[i].addEventListener('click', function (ele) {
-      debugger
-      var modalEle = ele.currentTarget.dataset.target
-      document.getElementById(modalEle).classList.add('open')
-    })
-  }
+function attachEventListner () {
+  document.addEventListener('click', function (evt) {
+    let eventType = evt.target.dataset.target
+    let targetEle = evt.target.dataset.href
+    switch (eventType) {
+      case 'modal':
+      modalHandler(targetEle)
+      break;
+    }
+  })
 }
+
+function modalHandler (targetEle) {
+  let ele = document.getElementById(targetEle)
+  ele.classList.contains('open') ? ele.classList.remove('open') : ele.classList.add('open')
+}
+
+// function attechEvtListnerOnOpenModal () {
+//   // var ele = document.getElementsByName('modal')
+//   // for (var i = 0; i < ele.length; i++) {
+//   //   ele[i].addEventListener('click', function (ele) {
+//   //     var modalEle = ele.currentTarget.dataset.target
+//   //     document.getElementById(modalEle).classList.add('open')
+//   //   })
+//   // }
+//   var a = document.getElementsByClassName('modal__button')
+//   a.addEventListener('click', function(ele) {
+//     debugger
+//   })
+// }
+
+// function attechEvtListnerOnCloseModal () {
+//   var ele = document.getElementsByName('close-modal')
+//   for (var i = 0; i < ele.length; i++) {
+//     ele[i].addEventListener('click', function (ele) {
+//       var modalEle = ele.currentTarget.dataset.target
+//       document.getElementById(modalEle).classList.remove('open')
+//     })
+//   }
+// }
