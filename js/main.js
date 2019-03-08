c2539172aaf38b51938a1c8b27c4f3fc6de36b6f
uday.sharma <uday.sharma@gdn-commerce.com>
2019-03-08 23:25:26 +0530

    removing commented code

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ window.addEventListener('load', function() {
 })
 
 function init () {
-  // attechEvtListnerOnOpenModal()
-  // attechEvtListnerOnCloseModal()
   attachEventListner()
 }
 
@@ -12,10 +10,12 @@ function attachEventListner () {
   document.addEventListener('click', function (evt) {
     let eventType = evt.target.dataset.target
     let targetEle = evt.target.dataset.href
-    switch (eventType) {
-      case 'modal':
-      modalHandler(targetEle)
-      break;
+    if (targetEle) {
+      switch (eventType) {
+        case 'modal':
+        modalHandler(targetEle)
+        break;
+      }
     }
   })
 }
@@ -24,27 +24,3 @@ function modalHandler (targetEle) {
   let ele = document.getElementById(targetEle)
   ele.classList.contains('open') ? ele.classList.remove('open') : ele.classList.add('open')
 }
-
-// function attechEvtListnerOnOpenModal () {
-//   // var ele = document.getElementsByName('modal')
-//   // for (var i = 0; i < ele.length; i++) {
-//   //   ele[i].addEventListener('click', function (ele) {
-//   //     var modalEle = ele.currentTarget.dataset.target
-//   //     document.getElementById(modalEle).classList.add('open')
-//   //   })
-//   // }
-//   var a = document.getElementsByClassName('modal__button')
-//   a.addEventListener('click', function(ele) {
-//     debugger
-//   })
-// }
-
-// function attechEvtListnerOnCloseModal () {
-//   var ele = document.getElementsByName('close-modal')
-//   for (var i = 0; i < ele.length; i++) {
-//     ele[i].addEventListener('click', function (ele) {
-//       var modalEle = ele.currentTarget.dataset.target
-//       document.getElementById(modalEle).classList.remove('open')
-//     })
-//   }
-// }
