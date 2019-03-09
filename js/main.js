bcf171ee4c5d1013b7be64adcfa158ae0e495523
NagaSruthi Kota <nagasruthikota@NagaSruthis-MacBook-Pro.local>
2019-03-09 15:29:14 +0530

    commit from sruthi

---
+++ js/main.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', function() {
+    init()
+})
+
+function init () {
+    attachEventListner()
+}
+
+function attachEventListner () {
+    document.addEventListener('click', function (evt) {
+        let eventType = evt.target.dataset.target
+        let targetEle = evt.target.dataset.href
+        if (targetEle) {
+            switch (eventType) {
+                case 'modal':
+                    modalHandler(targetEle)
+                    break;
+                case 'tabs':
+                    tabsHandler(targetEle, evt)
+            }
+        }
+    })
+}
+
+function tabsHandler(targetEle, evt) {
+    removeAddActive(evt.target.parentElement)
+    evt.target.classList.add('active')
+    removeAddActive(evt.target.parentElement.nextElementSibling)
+    let ele = document.getElementById(targetEle)
+    ele.classList.add('active')
+
+}
+function removeAddActive (parentEle) {
+    for (var i = 0; i < parentEle.children.length; i++) {
+        parentEle.children[i].classList.remove('active')
+    }
+}
+
